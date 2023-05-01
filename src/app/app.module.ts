e2f702f39db61f44a761ae7d245c1a1ebc4e19ae
DylanBenz2000 <dylanbenz2000@gmail.com>
2023-05-01 12:09:25 -0600

    Se agrega el proyecto a git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component';
+import { ApiComponent } from './components/api/api.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApiComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/api/api.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+@Component({
+  selector: 'app-api',
+  templateUrl: './api.component.html',
+  styleUrls: ['./api.component.css']
+})
+export class ApiComponent implements OnInit {
+
+  url: string = 'http://localhost:27017/refaccionaria'
+  error: any;
+
+  productos : any = [];
+  // producto: any = {
+  //   nombreProducto: '',
+  //   marcaProducto: '',
+  //   presentacionProducto: '',
+  //   contenidoProducto: '',
+  //   precioProducto:'',
+  //   proveedorProducto: '',
+  //   cantidadProductos: '',
+  //   estatusProducto: '',
+  //   descripcionProducto: '',
+  // }
+
+  constructor(private _http: HttpClient) { }
+
+  ngOnInit(): void {
+
+    this._http.get('http://localhost:3900/productos')
+    .subscribe(res =>{
+      this.productos = res;
+      console.log(this.productos)
+    })
+
+  }
+
+  // guardar(){
+  //   this._http.post('http://localhost:3900/producto/nuevo', this.producto)
+  //   .subscribe(res =>{
+  //     console.log(res)
+  //   });
+  // }
+
+}
